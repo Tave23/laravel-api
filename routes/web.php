cbda90ff965790b1c8191e687896cdc1218111cf
Davide Tavella <tavella01@gmail.com>
2022-02-09 17:12:57 +0100

    gestione rotte non in posts

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-})->name('home');
-
 Auth::routes();
 
 Route::middleware('auth')
@@ -32,3 +28,7 @@ Route::middleware('auth')
 
 
     });
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where('any', '.*')->name('home');
