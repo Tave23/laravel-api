4d4040104538395f6ff0ea2a87cb487b1cdfddcd
Davide Tavella <tavella01@gmail.com>
2022-02-09 16:08:25 +0100

    errore generazione file json con i post

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('prova', function(){
+//     return response()->json([
+
+//     ]);
+// });
+
+// rotta che va a puntare al controller
+Route::get('posts', 'Api\PostController@index');
