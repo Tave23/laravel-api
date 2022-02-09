ea7764aa99062ef7992e7c03ceed148f90b0a678
Davide Tavella <tavella01@gmail.com>
2022-02-09 17:06:46 +0100

    divisione file scss e js

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="{{ asset('css/guest/style.css') }}">
+   <title>Public Boolpress</title>
+</head>
+<body>
+   
+   <h2>
+      Home
+   </h2>
+
+
+   <script>{{ asset('js/guest.js') }}</script>
+</body>
+</html>
